094eafd0e3f04ff267c187b8062be174ed7bad32
Hasanda Fernando <hfernando@bglcorp.com.au>
2019-04-03 15:20:30 +1100

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,49 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit , OnInit} from '@angular/core';
+import { Select2OptionData } from 'ng2-select2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
-  ngAfterViewInit(){
-    /*$('#service_selector').selectize({
+  public exampleData: Array<Select2OptionData>;
+  public options: Select2Options;
 
-      onInitialize: function() {
-        //this.trigger('change', this.getValue(), true);
+
+
+  ngOnInit() {
+    this.exampleData = [
+      {
+        id: '475869',
+        text: 'Fund 01'
       },
-      onChange: function(value, isOnInitialize) {
-        console.log("Selectize event: ", value);
+      {
+        id: '89564849',
+        disabled: true,
+        text: 'Fund 02'
       },
-      maxItems: 1,
-      valueField: 'url',
-      searchField: 'title',
-      options: [
-        {id: 1, title: 'Portabilidad', excerpt: 'es cambiarte de operadora conservando tu número', url: 'https://diy.org'},
-        {id: 2, title: 'Linea Nueva', excerpt: 'es adquirir un número equipo y número', url: 'https://web.whatsapp.com/'},
-        {id: 3, title: 'Chip', excerpt: 'si ya tienes un equipo y solo te falta una linea', url: 'https://www.google.com'}
-      ],
-      render: {
-        option: function(data, escape) {
-          return '<div class="selectize-custom-option">' +
-            '<span class="selectize-custom-option-title">' + escape(data.title) + '</span>' +
-            '<span class="selectize-custom-option-excerpt">' + escape(data.excerpt) + '</span>' +
-            '</div>';
-        },
-        item: function(data, escape) {
-          return '<div class="selectize-custom-link"><div class="content-outer"><div class="content-inner"><span class="selectize-custom-link-title">' + escape(data.title) + '</span><span class="selectize-custom-link-excerpt">' + escape(data.excerpt) + '</span></div></div></div>';
-        }
+      {
+        id: '456586',
+        text: 'Fund 03'
       },
-      persist: false,
-      createOnBlur: false,
-      create: false
-    });*/
+      {
+        id: '8458603',
+        text: 'Fund 04'
+      }
+    ];
+
+    this.options = {
+      theme: 'form-control form-control-select2 form-control-select'
+    }
+
+  }
+
+  ngAfterViewInit(){
+
+
   }
 
 }
